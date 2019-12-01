f410ef7d2d651cc9edaf47109a36d4cdebc135f2
Poorna Jayasinghe <poornasjayasinghe@gmail.com>
2019-12-02 08:40:00 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MySQL Docker
+
+grab the generated password from the console, when the container running.
+
+
---
+++ root_password.sql
@@ -0,0 +1,5 @@
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+CREATE USER 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
